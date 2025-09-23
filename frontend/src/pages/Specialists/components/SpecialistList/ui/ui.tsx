f4ea3d3ca5a8423e0b8a15e5f88b1d55a2b41a2a
SenN1dko <amirraimovtop1@example.com>
2025-09-23 19:25:52 +0300

    Добавил еще адаптиа и сделал фильтрацию специалисто с карточкой

--- frontend/src/pages/Specialists/components/SpecialistList/ui/ui.tsx
+++ frontend/src/pages/Specialists/components/SpecialistList/ui/ui.tsx
@@ -1,131 +1,66 @@
-import c from './ui.module.scss'
+import c from './ui.module.scss';
+
+import { FilterDoctors } from './FilteredDoctors';
+import { useState } from 'react';
 
 const SpecialistList = () => {
+  const [selectedLetter, setSelectedLetter] = useState<string | null>(null);
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const alphabet = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯ'.split('');
+  return (
+    <div className={c.container}>
+      <div className={c.header}>
+        <span className={c.title}>Команда специалистов</span>
 
-    return (
-        <div className={c.container}>
-            <div className={c.header}>
-                <span className={c.title}>Команда специалистов</span>
+        <div className={c.options}>
+          <div className={c.wrapper}>
+            Пол
+            <img src="/arrow-bottom.svg" alt="" />
+          </div>
 
-                <div className={c.options}>
-                    <div className={c.wrapper}>
-                        Пол
-                        <img src="/arrow-bottom.svg" alt="" />
-                    </div>
+          <div className={c.wrapper}>
+            Специализация
+            <img src="/arrow-bottom.svg" alt="" />
+          </div>
 
-                    <div className={c.wrapper}>
-                        Специализация
-                        <img src="/arrow-bottom.svg" alt="" />
-                    </div>
-
-                    <div className={c.wrapper2}>
-                        <img src="/loop.svg" alt="" />
-                        <input placeholder='Поиск по названию услуги' className={c.input} />
-                    </div>
-                </div>
-            </div>
-
-            <div className={c.filters}>
-                <div className={c.filter}>Уролог <img src="/x.svg" className={c.x} alt="" /></div>
-
-                <div className={c.alpha}>
-                    <span className={c.all}>Все</span>
-                    А   Б  В  Г  Д   Е   Ё   Ж   З   И   К   Л   М   Н   О   П   Р   С   Т   У   Ф   Х   Ц   Ч   Ш   Щ   Ъ   Ы   Ь   Э   Ю   Я
-                </div>
-            </div>
-
-            <div  className={c.grid}>
-                <div className={c.block}>
-                    <div className={c.wrapper}>
-                        <img src="/first-doctor.png" alt="" />
-                        <div className={c.exp}>Стаж 31 год</div>
-                    </div>
-
-                    <div className={c.info}>
-                        <span className={c.name}>Нудельман <br/>Наталия Федоровна</span> 
-                        <span className={c.job}>Врач-невролог</span>
-
-                        <button className={c.button}>Записаться</button>
-                    </div>
-                </div>
-
-                
-                <div className={c.block}>
-                    <div className={c.wrapper}>
-                        <img src="/second-doctor.png" alt="" />
-                        <div className={c.exp}>Стаж 25 лет</div>
-                    </div>
-
-                    <div className={c.info}>
-                        <span className={c.name}>Хан<br/>Умар Хаят</span> 
-                        <span className={c.job}>Андролог, главный врач, уролог</span>
-
-                        <button className={c.button}>Записаться</button>
-                    </div>
-                </div>
-
-
-                <div className={c.block}>
-                    <div className={c.wrapper}>
-                        <img src="/third-doctor.png" alt="" />
-                        <div className={c.exp}>Стаж 27 лет</div>
-                    </div>
-
-                    <div className={c.info}>
-                        <span className={c.name}>Хан<br/>Тюльпан Тимергалиевна</span> 
-                        <span className={c.job}>Акушер-гинеколог, врач УЗИ, генеральный директор, гинеколог</span>
-
-                        <button className={c.button}>Записаться</button>
-                    </div>
-                </div>
-
-
-                <div className={c.block}>
-                    <div className={c.wrapper}>
-                        <img src="/fourth-doctor.png" alt="" />
-                        <div className={c.exp}>Стаж 15 лет</div>
-                    </div>
-
-                    <div className={c.info}>
-                        <span className={c.name}>Башарова<br/>Альбина Шарипзяновна</span> 
-                        <span className={c.job}>Врач УЗИ, врач функциональной диагностики, кардиолог, терапевт</span>
-
-                        <button className={c.button}>Записаться</button>
-                    </div>
-                </div>
-
-
-                <div className={c.block}>
-                    <div className={c.wrapper}>
-                        <img src="/fifth-doctor.png" alt="" />
-                        <div className={c.exp}>Стаж 6 лет</div>
-                    </div>
-
-                    <div className={c.info}>
-                        <span className={c.name}>Мартьянов <br/>Иван Сергеевич</span> 
-                        <span className={c.job}>Хирург</span>
-
-                        <button className={c.button}>Записаться</button>
-                    </div>
-                </div>
-
-
-                <div className={c.block}>
-                    <div className={c.wrapper}>
-                        <img src="/sixth-doctor.png" alt="" />
-                        <div className={c.exp}>Стаж 14 лет</div>
-                    </div>
-
-                    <div className={c.info}>
-                        <span className={c.name}>Юлдашев <br/>Фарход Талибович</span> 
-                        <span className={c.job}>Проктолог, эндоскопист</span>
-
-                        <button className={c.button}>Записаться</button>
-                    </div>
-                </div>
-            </div>
+          <div className={c.wrapper2}>
+            <img src="/loop.svg" alt="" />
+            <input
+              placeholder="Поиск по названию услуги"
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className={c.input}
+            />
+          </div>
         </div>
-    )
-}
+      </div>
 
-export default SpecialistList
+      <div className={c.filters}>
+        <div className={c.filter}>
+          Уролог <img src="/x.svg" alt="x" />
+        </div>
+
+        <div className={c.alpha}>
+          {alphabet.map((letter) => (
+            <button
+              key={letter}
+              className={`${c.letterBtn} ${
+                selectedLetter === letter ? c.active : ''
+              }`}
+              onClick={() =>
+                setSelectedLetter(selectedLetter === letter ? null : letter)
+              }
+            >
+              {letter}
+            </button>
+          ))}
+        </div>
+      </div>
+
+      <div className={c.doctorList}>
+        <FilterDoctors letter={selectedLetter ?? ''} />
+      </div>
+    </div>
+  );
+};
+
+export default SpecialistList;
