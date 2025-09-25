98f9a8660dbbaa45060cdb27f218d47858c7c9ce
Arthur <arturopro346@gmail.com>
2025-09-25 10:07:37 +0500

    fix host

--- backend/Medkhan/middleware.py
+++ backend/Medkhan/middleware.py
@@ -4,7 +4,10 @@ from fastapi.middleware.cors import CORSMiddleware
 
 def _add_cors_middleware(app: FastAPI) -> None:
     """Add CORS Middleware."""
-    app.add_middleware(CORSMiddleware, allow_origins=["*"])
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*", "http://91.147.93.29:8000", "http://91.147.93.29"],
+    )
 
 
 def add_middleware(app: FastAPI) -> None:
